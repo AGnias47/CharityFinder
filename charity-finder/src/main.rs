abafc84eb33ba4577dc801f3441b61b9157f5ffc
Andy Gnias <AGnias47@gmail.com>
2020-08-22 14:55:35 -0400

    Initial input

--- charity-finder/src/main.rs
+++ charity-finder/src/main.rs
@@ -1,3 +1,12 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+use text_io::read;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Which issue matters the most to you?");
+    println!("1. Criminal Justice Reform");
+    println!("2. Climate Change");
+    println!("3. Healthcare");
+    let choice: i32 = read!();
+    println!("You chose {}!", choice);
 }
